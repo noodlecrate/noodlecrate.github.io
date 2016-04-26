dcc777b958b3e28c52062a4081d0f0a4add9d738
jamesrichford <jamesrichford@googlemail.com>
2016-04-26 20:04:41 +0100

    Making things look interesting...

--- public/ts/components/app.ts
+++ public/ts/components/app.ts
@@ -1,4 +1,4 @@
-import { Component } from "angular2/core";
+import { Component, Inject } from "angular2/core";
 import { NgFor } from "angular2/common";
 import { bootstrap } from "angular2/platform/browser";
 import { Http, HTTP_PROVIDERS } from "angular2/http";
@@ -14,7 +14,7 @@ import { ArticleSummary } from "./article-summary";
 class SiteContainer {
    private _noodles: Array<any>;
 
-   public constructor(http: Http) {
+   public constructor(@Inject(Http) http: Http) {
       http
          .get("http://localhost:3000/noodles/")
          .map(x => x.json())
