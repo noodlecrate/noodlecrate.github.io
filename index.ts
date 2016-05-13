2bfa25a7bea1e490b75971ecc48f6262de335441
jamesrichford <jamesrichford@googlemail.com>
2016-05-13 08:42:18 +0100

    Get routes defaulting to SPA

--- index.ts
+++ index.ts
@@ -11,3 +11,8 @@ app.use("/node_modules", Express.static("node_modules"));
 http.createServer(app).listen(app.get("port"), () => {
   console.log("Express server listening on port " + app.get("port"));
 });
+
+//in case can't find path it's probably SPA so...
+app.use(function(req: any, res: any, next: any) {
+  res.status(200).sendFile("/public/index.html", { root: "./" });
+});
