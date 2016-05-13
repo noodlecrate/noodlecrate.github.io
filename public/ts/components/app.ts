749d707bcb3ecb57201b84ef4fdb4cd63fddfc09
jamesrichford <jamesrichford@googlemail.com>
2016-05-13 09:06:40 +0100

    Various bits of tidy up

--- public/ts/components/app.ts
+++ public/ts/components/app.ts
@@ -1,11 +1,13 @@
 import { Component, Inject, provide } from "angular2/core";
+import { APP_BASE_HREF } from "angular2/platform/common";
 import { NgFor } from "angular2/common";
 import { bootstrap } from "angular2/platform/browser";
 import { Http, HTTP_PROVIDERS } from "angular2/http";
 import "rxjs/rx";
 import { HomePage } from "./home-page";
+import { NotFoundPage } from "./not-found-page";
 import { ViewReviewPage } from "./view-review-page";
-import {ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from "angular2/router";
+import { ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from "angular2/router";
 
 @Component({
     directives: [ ROUTER_DIRECTIVES ],
@@ -16,9 +18,10 @@ import {ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from "angular2/router
                <router-outlet></router-outlet>`
 })
 @RouteConfig([
-    { path: "/", component: HomePage, name: "HomePage", useAsDefault: true },
+    { path: "/", component: HomePage, name: "HomePage" },
+    { path: "/not-found", component: NotFoundPage, name: "NotFoundPage", useAsDefault: true },
     { path: "/review/:reviewId", component: ViewReviewPage, name: "ViewReviewPage" }
 ])
 class SiteContainer {
 }
-bootstrap(SiteContainer, ROUTER_PROVIDERS);
+bootstrap(SiteContainer, [ ROUTER_PROVIDERS, provide(APP_BASE_HREF, {useValue: '/'}) ]);
---
+++ public/ts/components/not-found-page.ts
@@ -0,0 +1,9 @@
+import { Component } from "angular2/core";
+
+@Component({
+    selector: "not-found-page",
+    template: `<h1>Seems like you've stumbled off the beaten track</h1>
+               <p>Go back from whence you came!</p>`
+})
+export class NotFoundPage {
+}
