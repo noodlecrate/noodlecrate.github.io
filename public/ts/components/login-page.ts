8c8958b3e2e08195ff88e686365e34ba61459b03
James Richford <jamesrichford@googlemail.com>
2016-05-21 00:48:21 +0100

    Simulate login

--- public/ts/components/login-page.ts
+++ public/ts/components/login-page.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from "angular2/core";
 import { NgFor } from "angular2/common";
-import { Http, HTTP_PROVIDERS } from "angular2/http";
-import { InputComponent, ButtonComponent } from "feel-ui/feel-ui"
+import { Http, HTTP_PROVIDERS, Headers } from "angular2/http";
+import { InputComponent, ButtonComponent, NotificationProvider } from "feel-ui/feel-ui"
 import "rxjs/rx";
 
 @Component({
@@ -9,15 +9,32 @@ import "rxjs/rx";
     providers: [ HTTP_PROVIDERS ],
     selector: "login-page",
     template: `<form id="login-form">
-                  <feel-input [(value)]=username [label]="'Username'"></feel-input>{{username}}
+                  <feel-input [(value)]=username [label]="'Username'"></feel-input>
                   <feel-input type="password" [(value)]=password [label]="'Password'"></feel-input>
-                  <feel-button text="Sign in"></feel-button>
+                  <feel-button text="Sign in" primary=true (click)=signIn()></feel-button>
                </form>`
 })
 export class LoginPage {
    public username: string;
    public password: string;
+   private _http: Http;
 
-   public constructor() {
+   public constructor(http: Http) {
+     this._http = http;
+   }
+
+   public signIn() {
+     let headers = new Headers();
+     headers.append('Content-Type', 'application/json');
+
+     this._http.post("http://localhost:3000/session",
+        JSON.stringify({ username: this.username, password: this.password }),
+      { headers: headers })
+     .subscribe(() => {
+       new NotificationProvider().showSuccess("Hooray", "you got it right");
+     },
+     () => {
+       new NotificationProvider().showError("Boo", "you got it wrong");
+     });
    }
 }
