91ea695872f181e817abd7255e47669431465844
jamesrichford <jamesrichford@googlemail.com>
2016-08-03 19:03:55 +0100

    Move get all reviews to repository

--- public/ts/components/home-page.ts
+++ public/ts/components/home-page.ts
@@ -1,33 +1,24 @@
-import { Component, Inject } from "angular2/core";
+import { Component } from "angular2/core";
 import { NgFor } from "angular2/common";
 import { Http, HTTP_PROVIDERS } from "angular2/http";
-import "rxjs/Rx";
 import { ArticleSummary } from "./article-summary";
+import { ReviewRepository } from "../respositories/review-repository";
+import "rxjs/Rx";
 
 @Component({
     directives: [ NgFor, ArticleSummary ],
-    providers: [ HTTP_PROVIDERS ],
+    providers: [ HTTP_PROVIDERS, ReviewRepository ],
     selector: "home-page",
     template: `<article-summary *ngFor="let review of _reviews" [review]="review"></article-summary>`
 })
 export class HomePage {
     private _reviews: Array<any>;
 
-    public constructor(@Inject(Http) http: Http) {
-        // TODO: Use angular http when it supports withCredentials
+    public constructor(private _reviewRepository: ReviewRepository) {
+      this._getReviews();
+   }
 
-        let request = new XMLHttpRequest();
-        request.open("GET", "http://pp050:3000/reviews");
-
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.withCredentials = true;
-
-        request.onreadystatechange = function () {
-            if(request.readyState === XMLHttpRequest.DONE && request.status == 200) {
-                this._reviews = JSON.parse(request.responseText);
-            }
-        }.bind(this);
-
-        request.send();
+   private async _getReviews() {
+      this._reviews = await this._reviewRepository.getReviews();
    }
 }
